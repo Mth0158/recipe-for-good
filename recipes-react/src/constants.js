294e2afac95010bca59bd47d0a5f4abbe1bc3d86
Mth0158 <mathieu.eustachy@gmail.com>
2023-09-10 19:37:12 +0200

    [Bugfix] Correct api url (#14)

--- recipes-react/src/constants.js
+++ recipes-react/src/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = process.env.NODE_ENV === "test"
   ? "http://some-test-url"
-  : (import.meta.env.VITE_API_URL || "https://recipes-for-good.fly.dev/api/v1") // TODO: use properylu dotenv in production
+  : "https://recipes-for-good.fly.dev/api/v1" // TODO: use properylu dotenv in production
